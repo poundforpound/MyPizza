a8ae4dd582fea8bcca4a78e54a89535191068a7e
PoundforPound <a.zenit2014@yandex.ru>
2024-01-13 16:22:58 +0300

    Lesson1_end

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categoties() {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categoties;
